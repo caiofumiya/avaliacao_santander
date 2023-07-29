1be259bd7318e8178837e3ca10ee313279ce1122
Caio Fumiya <31969541+caiofumiya@users.noreply.github.com>
2023-07-29 17:45:18 -0300

    Adicionada navegação para páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ListagemUsuariosComponent } from './listagem-usuarios/listagem-usuarios.component';
+import { BuscarUsuarioComponent } from './buscar-usuario/buscar-usuario.component';
+import { CriarUsuarioComponent } from './criar-usuario/criar-usuario.component';
+import { AtualizarUsuarioComponent } from './atualizar-usuario/atualizar-usuario.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ListagemUsuariosComponent,
+    BuscarUsuarioComponent,
+    CriarUsuarioComponent,
+    AtualizarUsuarioComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/listagem-usuarios/listagem-usuarios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem-usuarios',
+  templateUrl: './listagem-usuarios.component.html',
+  styleUrls: ['./listagem-usuarios.component.scss']
+})
+export class ListagemUsuariosComponent {
+
+}
